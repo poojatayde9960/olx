e6adfb740826f92acb16f1236a8ca658f4bc0685
pooja <https://github.com/poojatayde9960/ecom.git>
2024-07-26 16:35:20 +0530

    initial setup

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,9 +3,9 @@ const User = require("../models/User")
 const sendEmail = require("../utils/email")
 
 exports.verifyUserEmail = asyncHandler(async (req, res) => {
-    const result = await User.findById(req.logggedInUser)
-    const otp = Math.floor(10000 + Math.random() * 900000)    //otp return return change honya sathi
-    await User.findByIdAndUpdate(req.logggedInUser, { emailCode: otp })   // ya line ni otp database mdhe jato
+    const result = await User.findById(req.loggedInUser)
+    const otp = Math.floor(10000 + Math.random() * 900000)                      //otp return return change honya sathi
+    await User.findByIdAndUpdate(req.loggedInUser, { emailCode: otp })   // ya line ni otp database mdhe jato
     sendEmail({
         to: result.email,
         subject: "Verify Email",
