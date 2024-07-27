f6d265aad10650930546551fc774c94c7c1f10a3
pooja <https://github.com/poojatayde9960/ecom.git>
2024-07-27 13:13:08 +0530

    user email and mobile otp fixed

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,6 +4,9 @@ const sendEmail = require("../utils/email")
 
 exports.verifyUserEmail = asyncHandler(async (req, res) => {
     const result = await User.findById(req.loggedInUser)
+    if (!result) {
+        return res.status(401).json({ message: "You are not logged In . Please Login Again" })
+    }
     const otp = Math.floor(10000 + Math.random() * 900000)                      //otp return return change honya sathi
     await User.findByIdAndUpdate(req.loggedInUser, { emailCode: otp })   // ya line ni otp database mdhe jato
     sendEmail({
@@ -13,4 +16,28 @@ exports.verifyUserEmail = asyncHandler(async (req, res) => {
     })
 
     res.json({ message: "verification Send Success" })
+})
+exports.verifyEmailOTP = asyncHandler(async (req, res) => {
+    const result = await User.findById(req.loggedInUser)
+    if (!result) {
+        return res.status(401).json({ message: "You are not logged In . Please Login Again" })
+    }
+    if (otp !== result.emailCode) {
+        return res.status(400).json({ message: "Invalid OTP" })
+
+    }
+    await User.findByIdAndUpdate(req.loggedInUser, { emailVerified: true })
+    res.json({ message: "Email Verify Success" })
+})
+exports.verifyMobileOTP = asyncHandler(async (req, res) => {
+    const result = await User.findById(req.loggedInUser)
+    if (!result) {
+        return res.status(401).json({ message: "You are not logged In . Please Login Again" })
+    }
+    if (otp !== result.mobileCode) {
+        return res.status(400).json({ message: "Invalid OTP" })
+
+    }
+    await User.findByIdAndUpdate(req.loggedInUser, { MobileVerified: true })
+    res.json({ message: "Mobile Verify Success" })
 })
